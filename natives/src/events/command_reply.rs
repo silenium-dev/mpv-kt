95bdefa46f23d92c75bf1171d42a27e5e0a824fb
silenium-dev <me@silenium-dev.net>
2026-05-14 01:09:01 +0200

    chore: remove obsolete struct

--- natives/src/events/command_reply.rs
+++ natives/src/events/command_reply.rs
@@ -1,10 +1,6 @@
 use crate::nodes::node::MpvNode;
 use libmpv2_sys::{mpv_event, mpv_event_command};
 
-struct EventCommandReply {
-    node: MpvNode,
-}
-
 pub(super) unsafe fn event_command_reply(raw: &mpv_event) -> MpvNode {
     unsafe {
         let raw_data = raw.data as *mut mpv_event_command;
