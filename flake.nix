ed3f8de58a9e3ca3d8c2edd63ca4f772fc42bba3
silenium-dev <me@silenium-dev.net>
2026-05-16 20:51:59 +0200

    chore: cleanup more things, move jni bindings to a separate module

--- flake.nix
+++ flake.nix
@@ -24,47 +24,48 @@
             ];
           };
           packages =
-          let
-            rustPlatform = pkgs.makeRustPlatform {
-              cargo = pkgs.rust-bin.nightly.latest.minimal;
-              rustc = pkgs.rust-bin.nightly.latest.minimal;
-            };
-          in
-          {
-            mpv-jni-rs = rustPlatform.buildRustPackage {
-              name = "mpv-jni-rs";
-              version = "0.1.0";
-              src = (builtins.path {
-                path = fs.toSource {
-                  root = ./natives;
-                  fileset = fs.unions [
-                    ./natives/src
-                    ./natives/Cargo.toml
-                    ./natives/Cargo.lock
-                  ];
-                };
-              });
-              cargoLock = {
-                lockFile = ./natives/Cargo.lock;
+            let
+              rustPlatform = pkgs.makeRustPlatform {
+                cargo = pkgs.rust-bin.nightly.latest.minimal;
+                rustc = pkgs.rust-bin.nightly.latest.minimal;
               };
-              useNextest = true;
-              buildType = "debug";
-              checkType = "debug";
-              nativeBuildInputs = with pkgs; [
-                which
-                cargo-nextest
-              ];
-              buildInputs = with pkgs; [
-                jdk21
-                mpv
-                llvmPackages.libclang
-              ];
-              LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.mpv ];
-              LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
-              NEXTEST_SUCCESS_OUTPUT = "immediate";
-              NEXTEST_FAILURE_OUTPUT = "immediate";
+            in
+            rec {
+              mpv-jni-rs = rustPlatform.buildRustPackage {
+                name = "mpv-jni-rs";
+                version = "0.1.0";
+                src = (builtins.path {
+                  path = fs.toSource {
+                    root = ./natives;
+                    fileset = fs.unions [
+                      ./natives/src
+                      ./natives/Cargo.toml
+                      ./natives/Cargo.lock
+                    ];
+                  };
+                });
+                cargoLock = {
+                  lockFile = ./natives/Cargo.lock;
+                };
+                useNextest = true;
+                buildType = "debug";
+                checkType = "debug";
+                nativeBuildInputs = with pkgs; [
+                  which
+                  cargo-nextest
+                ];
+                buildInputs = with pkgs; [
+                  jdk21
+                  mpv
+                  llvmPackages.libclang
+                ];
+                LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.mpv ];
+                LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+                NEXTEST_SUCCESS_OUTPUT = "immediate";
+                NEXTEST_FAILURE_OUTPUT = "immediate";
+              };
+              default = mpv-jni-rs;
             };
-          };
           devShells = {
             default = pkgs.mkShell {
               nativeBuildInputs = with pkgs; [
@@ -73,7 +74,6 @@
                 pkg-config
               ];
               buildInputs = with pkgs; [
-                packages.mpv-jni-rs
                 mpv
               ];
             };
--- natives/src/lib.rs
+++ natives/src/lib.rs
@@ -1,16 +1,7 @@
 #![feature(bstr)]
 
-use crate::events::callback::EventCallback;
-use crate::events::types::Event;
-use crate::mpv::Mpv;
-use crate::types::Format;
-use jni::objects::{JObject, JString};
-use jni::strings::JNIString;
-use jni::{jni_mangle, jni_sig, Env, EnvUnowned, JValue};
 use libmpv2_sys::mpv_node;
 use std::ffi::c_void;
-use std::thread::sleep;
-use std::time::Duration;
 
 #[macro_use]
 mod macros;
@@ -20,6 +11,7 @@ mod handle;
 pub mod mpv;
 mod test_utils;
 pub mod types;
+pub mod jni_bindings;
 
 pub(crate) unsafe fn mpv_free(data: *mut c_void) {
     #[cfg(not(test))]
@@ -37,58 +29,4 @@ pub(crate) unsafe fn mpv_free_node_contents(node: *mut mpv_node) {
     }
     #[cfg(test)]
     test_utils::mpv_free_node_contents_stub(node);
-}
-
-struct TestCallback;
-impl EventCallback for TestCallback {
-    fn on_event(&self, _jni_env: &mut Env, event: &Event) {
-        println!("{:?}", event);
-    }
-}
-
-#[jni_mangle("Test_main$TestNatives", "testN")]
-pub fn testN<'local>(mut env: EnvUnowned<'local>, _this: JObject<'local>) {
-    env.with_env(|env| {
-        println!("testN");
-        let mpv = Mpv::new(env, Box::new(TestCallback)).expect("Failed to create mpv");
-        mpv.initialize().expect("Failed to initialize mpv");
-        let request_id = mpv
-            .get_property_async("track-list", Format::NODE)
-            .expect("Failed to get property");
-        println!("request_id: {}", request_id);
-        let request_id = mpv
-            .get_property_async("pid", Format::NODE)
-            .expect("Failed to get property");
-        println!("request_id: {}", request_id);
-        let request_id = mpv
-            .get_property_async("mpv-version", Format::OSD_STRING)
-            .expect("Failed to get property");
-        println!("request_id: {}", request_id);
-        let request_id = mpv
-            .set_property_async("hwdec", "vaapi".into())
-            .expect("Failed to set property");
-        println!("request_id: {}", request_id);
-        sleep(Duration::from_secs(1));
-        let request_id = mpv
-            .get_property_async("hwdec", Format::OSD_STRING)
-            .expect("Failed to get property");
-        println!("request_id: {}", request_id);
-        sleep(Duration::from_secs(1));
-        mpv.terminate();
-
-        let class = env
-            .find_class(JNIString::new("Test_main$TestNatives"))
-            .expect("Failed to find class");
-        let str = JString::from_str(env, "hello from rust").expect("Failed to create string");
-        env.call_static_method(
-            class,
-            JNIString::new("test"),
-            jni_sig!("(Ljava/lang/String;)V"),
-            &[JValue::Object(&str)],
-        )
-        .expect("Failed to call test");
-
-        Ok::<(), jni::errors::Error>(())
-    })
-    .resolve::<jni::errors::ThrowRuntimeExAndDefault>()
 }
