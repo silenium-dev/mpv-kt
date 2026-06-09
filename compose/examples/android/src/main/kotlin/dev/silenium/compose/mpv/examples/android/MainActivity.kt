124bf187aebd5167623745d76c3215cdf1654a12
silenium-dev <me@silenium-dev.net>
2026-06-10 00:28:24 +0200

    feat: demo composable over Video Surface

--- compose/examples/android/src/main/kotlin/dev/silenium/compose/mpv/examples/android/MainActivity.kt
+++ compose/examples/android/src/main/kotlin/dev/silenium/compose/mpv/examples/android/MainActivity.kt
@@ -5,10 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import dev.silenium.libs.mpv.compose.VideoSurface
 import dev.silenium.mpv.Mpv
 import dev.silenium.mpv.native_bindings.node.Node
@@ -37,6 +44,16 @@ class MainActivity : ComponentActivity() {
                         mpv.commandAsync("loadfile", testVideo.absolutePath).getOrThrow()
                     }
                 })
+                Surface(
+                    color = Color.White.copy(alpha = 0.8f),
+                    shape = MaterialTheme.shapes.small,
+                ) {
+                    Text(
+                        text = "Composable over Video Surface",
+                        fontSize = 24.sp,
+                        modifier = Modifier.padding(8.dp),
+                    )
+                }
             }
         }
     }
