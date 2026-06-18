1e45796a9f346319f7bcef417bcf8253fe005b3c
silenium-dev <me@silenium-dev.net>
2026-06-18 18:35:32 +0200

    ci: unify ci settings

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,18 +1,15 @@
-import jetbrains.buildServer.configs.kotlin.BuildSteps
-import jetbrains.buildServer.configs.kotlin.BuildType
-import jetbrains.buildServer.configs.kotlin.DslContext
-import jetbrains.buildServer.configs.kotlin.ParameterDisplay
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.BuildStep.ExecutionMode
 import jetbrains.buildServer.configs.kotlin.buildFeatures.commitStatusPublisher
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
 import jetbrains.buildServer.configs.kotlin.buildFeatures.pullRequests
+import jetbrains.buildServer.configs.kotlin.buildFeatures.vcsLabeling
 import jetbrains.buildServer.configs.kotlin.buildSteps.gitHubRelease
 import jetbrains.buildServer.configs.kotlin.buildSteps.gradle
 import jetbrains.buildServer.configs.kotlin.buildSteps.script
-import jetbrains.buildServer.configs.kotlin.project
 import jetbrains.buildServer.configs.kotlin.projectFeatures.UntrustedBuildsSettings
 import jetbrains.buildServer.configs.kotlin.projectFeatures.untrustedBuildsSettings
 import jetbrains.buildServer.configs.kotlin.triggers.vcs
-import jetbrains.buildServer.configs.kotlin.version
 
 version = "2026.1"
 
@@ -45,6 +42,15 @@ object BuildRelease : BuildType({
     features {
         perfmon {
         }
+
+        vcsLabeling {
+            vcsRootId = "${DslContext.settingsRoot.id}"
+            labelingPattern = "%release.version%"
+            successfulOnly = true
+            branchFilter = """
+                |+:*
+            """.trimMargin()
+        }
     }
 
     requirements {
@@ -59,21 +65,54 @@ object BuildRelease : BuildType({
             display = ParameterDisplay.PROMPT,
             allowEmpty = false
         )
+
         text(
-            "deploy.repo-url",
+            "nexus.repo-url",
             "https://nexus.silenium.dev/repository/maven-releases",
             display = ParameterDisplay.HIDDEN,
             readOnly = true
         )
         text(
-            "deploy.username",
+            "nexus.username",
             "teamcity-ci",
             display = ParameterDisplay.HIDDEN,
             readOnly = true
         )
         password(
-            "deploy.password",
+            "nexus.password",
             "credentialsJSON:149ec97d-3f03-4588-b740-38f933c0d1e2",
+            display = ParameterDisplay.HIDDEN,
+            readOnly = true
+        )
+
+        password(
+            "maven-central.username",
+            "credentialsJSON:252356db-9418-4ae9-8f06-0d16bc690805",
+            display = ParameterDisplay.HIDDEN,
+            readOnly = true
+        )
+        password(
+            "maven-central.password",
+            "credentialsJSON:6375fac2-1d65-4f4e-bfea-09191d89f44d",
+            display = ParameterDisplay.HIDDEN,
+            readOnly = true
+        )
+
+        password(
+            "gpg.secret-key",
+            "credentialsJSON:aa15cd06-be04-40d6-9569-a781b94f5d9c",
+            display = ParameterDisplay.HIDDEN,
+            readOnly = true
+        )
+        password(
+            "gpg.public-key",
+            "credentialsJSON:03a34fba-18cb-4875-8996-135df4d49efd",
+            display = ParameterDisplay.HIDDEN,
+            readOnly = true
+        )
+        password(
+            "gpg.passphrase",
+            "credentialsJSON:becb8d56-5e47-4e7f-847c-166dcaae1a34",
             display = ParameterDisplay.HIDDEN,
             readOnly = true
         )
@@ -83,20 +122,36 @@ object BuildRelease : BuildType({
         with(Nix) {
             loadNixEnv()
         }
-        gradle {
-            tasks = """
-                |build
-                |publish
-            """.trimMargin().replace("\n", " ")
-            gradleParams = """
+        val gradleArgs = """
                 |-Pdeploy.version=%release.version%
                 |-Pdeploy.enabled=true
                 |-Pnexus.enabled=true
-                |-Pnexus.repo-url=%deploy.repo-url%
-                |-Pnexus.username=%deploy.username%
-                |-Pnexus.password=%deploy.password%
+                |-Pnexus.repo-url=%nexus.repo-url%
+                |-Pnexus.username=%nexus.username%
+                |-Pnexus.password=%nexus.password%
+                |-Pmaven-central.enabled=true
+                |-Pmaven-central.username=%maven-central.username%
+                |-Pmaven-central.password=%maven-central.password%
+                |-Pgpg.secret-key=%gpg.secret-key%
+                |-Pgpg.public-key=%gpg.public-key%
+                |-Pgpg.passphrase=%gpg.passphrase%
+                |-Pgpg.enabled=true
+                |--scan
                 |--info
             """.trimMargin().replace("\n", " ")
+        gradle {
+            tasks = """
+                |clean
+                |build
+                |publish
+            """.trimMargin().replace("\n", " ")
+            gradleParams = gradleArgs
+        }
+        gradle {
+            tasks = """
+                |jreleaserDeploy
+            """.trimMargin().replace("\n", " ")
+            gradleParams = gradleArgs
         }
         gitHubRelease {
             name = "Create GitHub Release"
@@ -143,19 +198,19 @@ object BuildSnapshot : BuildType({
 
     params {
         text(
-            "deploy.repo-url",
+            "nexus.repo-url",
             "https://nexus.silenium.dev/repository/maven-snapshots",
             display = ParameterDisplay.HIDDEN,
             readOnly = true
         )
         text(
-            "deploy.username",
+            "nexus.username",
             "teamcity-ci",
             display = ParameterDisplay.HIDDEN,
             readOnly = true
         )
         password(
-            "deploy.password",
+            "nexus.password",
             "credentialsJSON:149ec97d-3f03-4588-b740-38f933c0d1e2",
             display = ParameterDisplay.HIDDEN,
             readOnly = true
@@ -175,9 +230,10 @@ object BuildSnapshot : BuildType({
                 |-Pci=true
                 |-Pdeploy.enabled=true
                 |-Pnexus.enabled=true
-                |-Pnexus.repo-url=%deploy.repo-url%
-                |-Pnexus.username=%deploy.username%
-                |-Pnexus.password=%deploy.password%
+                |-Pnexus.repo-url=%nexus.repo-url%
+                |-Pnexus.username=%nexus.username%
+                |-Pnexus.password=%nexus.password%
+                |--scan
                 |--info
             """.trimMargin().replace("\n", " ")
         }
@@ -226,6 +282,7 @@ object BuildPR : BuildType({
             """.trimMargin().replace("\n", " ")
             gradleParams = """
                 |-Pdeploy.enabled=false
+                |--scan
                 |--info
             """.trimMargin().replace("\n", " ")
         }
